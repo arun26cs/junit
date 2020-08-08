4d440232fbc172ca799a410a28bd64049f6ab87e
arun varghese <arun26cs@gmail.com>
2020-08-08 02:40:31 -0400

    @Beforeall and other hooks

--- src/test/java/junittest/MathutilsTest.java
+++ src/test/java/junittest/MathutilsTest.java
@@ -2,13 +2,25 @@ package junittest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathutilsTest {
 
+	Mathutils mathutil;
+	
+	@BeforeAll
+	static void intiall() {
+		System.out.println("before all");
+	}
+	@BeforeEach
+	void init() {
+		mathutil=new Mathutils();
+	}
 	@Test
 	void test() {
-		Mathutils mathutil = new Mathutils();
+		//Mathutils mathutil = new Mathutils();
 		int expected = 2;
 		int actual=mathutil.add(1, 1);
 		assertEquals(expected, actual);
@@ -17,7 +29,7 @@ class MathutilsTest {
 	}
 	@Test
 	void testDiv() {
-		Mathutils mathutil = new Mathutils(); 
+		//Mathutils mathutil = new Mathutils(); 
 		//System.out.println(mathutil.div(1,0));
 		assertThrows(ArithmeticException.class, ()-> mathutil.div(1,0),"Divide by zero eror");
 		
