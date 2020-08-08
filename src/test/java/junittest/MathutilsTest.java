486f738f0d0112961a2da527df12e02a4b166f81
arun varghese <arun26cs@gmail.com>
2020-08-08 02:48:13 -0400

    @Beforeall can excute as non static usinf testinstance annotation as per class

--- src/test/java/junittest/MathutilsTest.java
+++ src/test/java/junittest/MathutilsTest.java
@@ -5,13 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathutilsTest {
 
 	Mathutils mathutil;
 	
 	@BeforeAll
-	static void intiall() {
+	void intiall() {
 		System.out.println("before all");
 	}
 	@BeforeEach
